29f2244319e7fa4814596ad76c679d74c1ad320c
jessfraser8 <jfraser4@usf.edu>
2024-06-26 17:20:53 -0400

    Create navigation buttons for product display.

--- script.js
+++ script.js
@@ -1,42 +1,70 @@
 // U09226075
+
+productData = []
+length = 0
+let index = 0
+
+// Display next button.
+function onNext() {
+    index++
+
+    if (index >= length) {
+        index = 0
+    }
+
+    displayPageData()
+
+}
+
+// Display previous button.
+function onPrevious() {
+    index--
+
+    if (index < 0) {
+        index = length -1
+    }
+    
+    displayPageData()
+
+}
+
+async function sleep(milli) {
+    return new Promise(resolve => setTimeout(resolve, milli))
+}
+
+// Display data.
+function displayPageData() {
+    const product = productData[index]
+
+    const nameElement = document.getElementById("name");
+    nameElement.textContent = "Name: " + product.name;
+
+    const imageElement = document.getElementById("image");
+    imageElement.setAttribute("src", product.image)
+
+    const priceElement = document.getElementById("price");
+    priceElement.textContent = "Price: " + "$" + product.price;
+
+    const descriptionElement = document.getElementById("description");
+    descriptionElement.textContent = "Description: " + product.description;
+}
+
+// Fetch and display data.
 function main(){
-
-    console.log("I am alive!");
-    const request = fetch("products.json")
+    
+    const request = fetch("https://www.course-api.com/react-store-products")
     .then(response => response.json())
-    .then((data)=>{
-        console.log(data)
-        for (index in data) {
-            const product = data[index]
-            
-        const dataDisplay = document.getElementById("dataDisplay");
+    .then(async function (data) {
+        await sleep (2000)
+        let loading = document.getElementById("loading")
+        loading.textContent = '';
+        length = data.length
+    
+        productData = data
 
-            const nameElement = document.createElement("p");
-            nameElement.textContent = "Name: " + product.name;
+        displayPageData()
 
-            const imageElement = document.createElement("p");
-            imageElement.textContent = "Image: " + product.image;
-
-            const priceElement = document.createElement("p");
-            priceElement.textContent = "Price: " + product.price;
-
-            const descriptionElement = document.createElement("p");
-            descriptionElement.textContent = "Description: " + product.description;
-
-            dataDisplay.appendChild(nameElement);
-            dataDisplay.appendChild(imageElement);
-            dataDisplay.appendChild(priceElement);
-            dataDisplay.appendChild(descriptionElement);
-
+        // Add error catch.
     }).catch((error) =>{alert(error);})
-
-
-
-
-
-
-
-
-
 
 }
